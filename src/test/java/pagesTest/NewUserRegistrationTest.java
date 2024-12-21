ae99ca94606a045faa77984b28d0d4601beec5cc
TofaelKabir <tofaelkabir@gmail.com>
2024-12-21 12:39:55 -0500

    tdd03

--- src/test/java/pagesTest/NewUserRegistrationTest.java
+++ src/test/java/pagesTest/NewUserRegistrationTest.java
@@ -2,6 +2,7 @@ package pagesTest;
 
 import static common.CommonActions.clearTextFromTheField;
 import static common.CommonActions.clickElement;
+import static common.CommonActions.clickElementThenTab;
 import static common.CommonActions.elementEnabled;
 import static common.CommonActions.inputText;
 import static common.CommonActions.inputTextThenClickTab;
@@ -50,14 +51,7 @@ public class NewUserRegistrationTest extends BaseClass {
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-	}
-	
-	@Test
-	public void step_two_title_and_header_validation_test() {
-		homePage.click_to_new_user_registration();
-		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
-		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.step_two_title_and_header_validation();
+		newUserRegistration.step_two_title_and_header();
 	}
 	
 	@Test
@@ -86,69 +80,218 @@ public class NewUserRegistrationTest extends BaseClass {
 	}
 	
 	@Test
-	public void first_name_validation_test() {
+	public void first_name_test() {
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.first_name_validation();
+		newUserRegistration.first_name();
 	}
 	
 	@Test
-	public void middle_name_validation_test() {
+	public void middle_name_test() {
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.middle_name_validation();
+		newUserRegistration.middle_name();
 	}
 	
 	@Test
-	public void last_name_validation_test() {
+	public void last_name_test() {
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.last_name_validation();
+		newUserRegistration.last_name();
 	}
 	
 	@Test
-	public void suffix_validation_test() {
+	public void suffix_test() {
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.suffix_validation();	
+		newUserRegistration.suffix();	
 	}
 	
 	@Test
-	public void birth_month_validation_test() {	
+	public void birth_month_test() {	
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.birth_month_validation();
+		newUserRegistration.birth_month();
 	}
 	
 	@Test
-	public void birth_year_validation_test() {	
+	public void birth_year_test() {	
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.birth_year_validation();
+		newUserRegistration.birth_year();
 	}
 	
 	@Test
-	public void birth_date_validation_test() {	
+	public void birth_date_test() {	
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.birth_date_validation();
+		newUserRegistration.birth_date();
 	}
 	
 	@Test
-	public void untilClickOnNextButtonInStep2WithData_test() {
+	public void address_line1_test () {
 		homePage.click_to_new_user_registration();
 		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
 		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
-		newUserRegistration.untilClickOnNextButtonInStep2WithData();
+		newUserRegistration.address_line1();
+	}
+	
+	@Test
+	public void address_line2_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.address_line2();
+	}
+	
+	@Test
+	public void city_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.city();
+	}
+	
+	@Test
+	public void state_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.state();
+	}
+	
+	@Test
+	public void zip_code_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.zip_code();
+	}
+	
+	@Test
+	public void zip_code_plus4_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.zip_code_plus4();
+	}
+	
+	@Test
+	public void email_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.email();
+	}
+	
+	@Test
+	public void confirm_email_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.confirm_email();
+	}
+	
+	@Test
+	public void phone_number_test () {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.phone_number();
+	}
+	
+	@Test
+	public void click_on_next_button_of_step_two_with_data_direct_to_step_three_test(){
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
 		newUserRegistration.click_on_next_button_of_step_two();
-		newUserRegistration.stepThreeTitleAndHeaderValidation();
+	}
+	
+	@Test
+	public void step_three_title_and_header_test() {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
+		newUserRegistration.click_on_next_button_of_step_two();
+		newUserRegistration.step_three_title_and_header();
+	}
+	
+	@Test
+	public void click_on_back_button_on_step_three_direct_to_step_two_test() {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
+		newUserRegistration.click_on_next_button_of_step_two();
+		newUserRegistration.click_on_back_button_on_step_three_direct_to_step_two();
+	}
+	
+	@Test
+	public void click_on_cancel_button_on_step_three_direct_to_homepage_test() {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
+		newUserRegistration.click_on_next_button_of_step_two();
+		newUserRegistration.click_on_cancel_button_on_step_three_direct_to_homepage();
+	}
+	
+	@Test
+	public void click_on_next_button_on_step_three_without_any_data_test() {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
+		newUserRegistration.click_on_next_button_of_step_two();
+		newUserRegistration.click_on_next_button_of_step_three();
+		newUserRegistration.error_message_under_and_top_of_the_page_on_step_three();
+	}	
+	
+	@Test
+	public void click_on_next_button_of_step_three_with_data_direct_to_summary_page_test(){
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
+		newUserRegistration.click_on_next_button_of_step_two();
+		newUserRegistration.happy_path_step_three();
+		newUserRegistration.click_on_next_button_of_step_three();
+	}
+	
+	@Test
+	public void summary_page_title_and_header_test() {
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
+		newUserRegistration.click_on_next_button_of_step_two();
+		newUserRegistration.happy_path_step_three();
+		newUserRegistration.click_on_next_button_of_step_three();
+		newUserRegistration.summary_page_title_and_header();
+	}
+	
+	@Test
+	public void click_on_submit_user_button_of_summary_page_direct_to_success_message_test(){
+		homePage.click_to_new_user_registration();
+		newUserRegistration.select_your_application_on_new_user_registration_page_step_one();
+		newUserRegistration.click_on_next_button_of_step_one_with_agree_to_terms_direct_to_step_two();
+		newUserRegistration.happy_path_step_two();
+		newUserRegistration.click_on_next_button_of_step_two();
+		newUserRegistration.happy_path_step_three();
+		newUserRegistration.click_on_next_button_of_step_three();
+		newUserRegistration.summary_page_title_and_header();
+		newUserRegistration.click_on_submit_user_button_of_summary_page();
+		newUserRegistration.success_message();
 	}
 	
 	
